93dcf5f7cdf894b74142a274fc96c2a57f679e9e
Jamie Kwong <jkwong@C02FC223MD6R-Jamie-Kwong.local>
2026-04-07 11:55:21 -0400

    Deploy 2026-04-07 11:55

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,6 +6,7 @@ import { useRef } from "react";
 import ClickBlobs from "@/components/ClickBlobs";
 import { useTheme } from "@/components/ThemeProvider";
 import PixelSparkles, { Star4, Star4Small, DotPixel } from "@/components/PixelSparkles";
+import ClickWord from "@/components/ClickWord";
 
 const STATEMENT = {
   text: "Design that moves people forward.",
@@ -146,7 +147,7 @@ export default function Hero() {
               >
                 {STATEMENT.accentLine1}
                 <br />
-                {STATEMENT.accentLine2}
+                {"one "}<ClickWord>click</ClickWord>{" at a time."}
               </motion.span>
             </span>
           </motion.h1>
